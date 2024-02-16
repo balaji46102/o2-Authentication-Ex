3cb1938648234dabefe35f3bc89b67474fc4d4c0
balaji46102 <balajiin99@gmail.com>
2024-02-16 11:23:08 +0530

    uplodaing o2auth files

---
+++ src/main/java/com/stubizsolutions/Ss28McrSrVO2AuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.stubizsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss28McrSrVO2AuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ss28McrSrVO2AuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stubizsolutions/Ss28McrSrVO2AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stubizsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss28McrSrVO2AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
